c1fde721e436579c0d4af9dc1537ed023b108176
Jordan Reed <reed5204@vandals.uidaho.edu>
2024-11-04 08:06:19 -0800

    added code to handle moving 1099 to correct spot and back

--- execute_me.sh
+++ execute_me.sh
@@ -10,13 +10,15 @@ if id "$username" >/dev/null 2>&1; then
     # delete files
     echo 'Scenario complete. Cleaning up files...'
 
+    # remove incriminating document
+    mv /home/$username/Documents/1099-for-kellogg-job.png ./.hidden/
     # remove username
     userdel -rf $username
     # remove hidden password file
     mv /tmp/$passwordFileName ./.hidden/
     # remove line from host name
     head -n -1 /etc/hosts > /etc/temphosts; mv /etc/temphosts /etc/hosts
-
+    
     echo "File clean up complete!"
 else
     # create files
@@ -27,6 +29,10 @@ else
     useradd -c "Timothy Lybeck" -d /home/$username/ -m -p "C3r3alK1ll3r" cerealk
     # create trash folder if not there
     mkdir -p /home/$username/.local/share/Trash
+    # create Documents folder if not there
+    mkdir -p /home/$username/Documents
+    # move incriminating document
+    mv ./.hidden/1099-for-kellogg-job.png /home/$username/Documents
     # create hidden password file
     mv ./.hidden/$passwordFileName /tmp/
     # create host name addition
